e8feef7aa299634e61541f3db6fe2f47700e34f0
Ayush Yadav <ayushyadav935@gmail.com>
2021-12-18 22:51:35 +0530

    Google analytics added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Spi from "./components/SPI/Spi";
 import Cpi from "./components/CPI/Cpi";
+import ReactGA from 'react-ga';
+import { useEffect } from "react";
+
+ReactGA.initialize('UA-215631298-1');
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     <Router>
       <Switch>
